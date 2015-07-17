88484bd63763da47e1c6a5486313284dc491b0d7
Mushtaq Ahmed <mushtaq.a@gmail.com>
2015-07-17 15:52:05 +0530

    fanout vs multicast

--- jvm/src/test/scala/rs/PushTest.scala
+++ jvm/src/test/scala/rs/PushTest.scala
@@ -1,8 +1,9 @@
 package rs
 
-import akka.stream.scaladsl.{Flow, Broadcast, FlowGraph, Source}
+import akka.stream.scaladsl._
 import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
 import org.scalatest.BeforeAndAfterAll
+import rs.library.FlowExtensions.RichFlow
 import rs.library.RsSuite
 import rs.library.SourceExtensions.RichSource
 import rs.library.Utils._
@@ -31,19 +32,48 @@ class PushTest extends RsSuite with BeforeAndAfterAll {
     it("unicast") {
       fork(numberStream)
     }
-    it("multicast") {
+    it("fanout") {
       fork(numberStream.multicast)
+    }
+    it("multicast") {
+      multicastFork(numberStream)
     }
   }
 
+  val sink = Flow[Int].take(10).toMat(Sink.ignore)(Keep.right)
+  val squaringFlow = Flow[Int].throttle(200.millis).map(squaring).map(squared)
+  val doublingFlow = Flow[Int].map(doubling).map(doubled)
 
   def fork(xs: Source[Int, Any]) = {
+    def branch(flow: Flow[Int, Int, Any]) = xs.via(flow).take(10).runWith(Sink.ignore)
+
     Thread.sleep(500)
     separator()
-    val future1 = xs.throttle(200.millis).map(squaring).map(squared).take(10).runForeach(ignore)
+    val future1 = branch(squaringFlow)
+
     Thread.sleep(500)
     separator()
-    val future2 = xs.map(doubling).take(10).runForeach(ignore)
+    val future2 = branch(doublingFlow)
+
+    await(future1.flatMap(_ => future2))
+  }
+
+  def multicastFork(xs: Source[Int, Any]) = {
+    val (future1, future2) = FlowGraph.closed(sink, sink)(Keep.both) { implicit b => (sink1, sink2) =>
+      import FlowGraph.Implicits._
+
+      val broadcast = b.add(Broadcast[Int](2))
+      xs ~> broadcast.in
+
+      separator()
+      Thread.sleep(500)
+      broadcast.out(0) ~> squaringFlow ~> sink1
+
+      separator()
+      Thread.sleep(500)
+      broadcast.out(1) ~> doublingFlow ~> sink2
+    }.run()
+
     await(future1.flatMap(_ => future2))
   }
 
